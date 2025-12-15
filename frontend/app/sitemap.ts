8410f37d19487f8bd636e3906ee68569bbe1f83d
Gregor <gregor.boznar@gmail.com>
2025-12-15 21:52:02 +0100

    feat: updated sitemap

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -2,29 +2,78 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://www.intenia-engineering.si'
-  
+  const lastModified = new Date()
+
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/products`,
-      lastModified: new Date(),
+      url: `${baseUrl}/en`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/fr`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/izdelki`,
+      lastModified,
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/gallery`,
-      lastModified: new Date(),
+      url: `${baseUrl}/en/products`,
+      lastModified,
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/cookies`,
-      lastModified: new Date(),
+      url: `${baseUrl}/fr/produits`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/galerija`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/en/gallery`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/fr/galerie`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/piskotki`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/en/cookies`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/fr/cookies`,
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.3,
     },
